cb2cc4e3a5f79bbd102b8112f82223f1fac3b299
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-08-21 13:57:23 +0400

    Initial commit

---
+++ bin/dart_application_2.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main(){
+
+  // String x="Hello In Dart";
+  // print(x.replaceAll(" ","" ));
+
+  // print("Please enter your age");
+  // String? s=stdin.readLineSync();
+  // int age = int.tryParse(s?? "0")?? 0;
+  // print(age);
+
+
+
+   
+var code;
+   code = code ?? "Java";
+   print(code);
+ 
+   var companyName = "Microsoft";
+   companyName = companyName ?? "Google";
+   print(companyName);
+
+}
+
